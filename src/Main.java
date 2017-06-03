1cdd9e447f052f867bc34a0dee50e5e0c1c4bef4
Şeyma Yılmaz <seyma.yilmaz3@ogr.sakarya.edu.tr>
2017-06-03 21:58:08 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Şeyma Yılmaz on 3.6.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList arrayList=new ArrayList();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(6);
+        arrayList.add(5);
+
+        System.out.println();
+    }
+
+}
